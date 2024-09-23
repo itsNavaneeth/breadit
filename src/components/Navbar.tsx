5b70c62d8c6f186858977d7e9ee60a760da2f904
Navaneeth S <navaneeth.suresh@moneyview.in>
2024-09-23 23:09:45 +0530

    auth completion 1:20:49

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,8 +2,10 @@ import Link from "next/link";
 import React from "react";
 import { Icons } from "./Icons";
 import { buttonVariants } from "./ui/Button";
+import { getAuthSession } from "@/lib/auth";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const session = await getAuthSession();
   return (
     <div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2">
       <div className="container max-w-7xl h-full mx-auto flex items-center justify-between gap-2">
@@ -13,9 +15,13 @@ const Navbar = () => {
           <p className="hidden text-zinc-700 text-sm font-medium md:block">Breadit</p>
         </Link>
 
-        <Link href="/sign-in" className={buttonVariants()}>
-          Sign In
-        </Link>
+        {session ? (
+          <p>youre signed in</p>
+        ) : (
+          <Link href="/sign-in" className={buttonVariants()}>
+            Sign In
+          </Link>
+        )}
       </div>
     </div>
   );
